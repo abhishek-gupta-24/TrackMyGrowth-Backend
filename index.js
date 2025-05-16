86e8a15784141f8452d5f1a1791df395e64da4fb
Abhishek Gupta <abhishek.gupta.150803@gmail.com>
2025-05-16 11:54:15 +0530

    backend to frontend

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: 'https://track-my-growth-frontend.vercel.app/',
+  origin: 'https://track-my-growth-frontend.vercel.app',
   credentials: true
 }));
 
