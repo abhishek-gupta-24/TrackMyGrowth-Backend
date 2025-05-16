e449c24826a8eb5ea45b8567a4feba40d54fd4ec
Abhishek Gupta <abhishek.gupta.150803@gmail.com>
2025-05-16 11:44:35 +0530

    backend to frontend connection

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors'; // 👉 add this line
 
 import authRoutes from './routes/auth.js';
-import updateRoutes from './routes/update.js'
-import fetchRoutes from './routes/fetch.js'
-import statsRoutes from './routes/apiCalls.js'
+import updateRoutes from './routes/update.js';
+import fetchRoutes from './routes/fetch.js';
+import statsRoutes from './routes/apiCalls.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+app.use(cors({
+  origin: 'https://track-my-growth-frontend.vercel.app/',
+  credentials: true
+}));
 
 app.use(express.json());
 
@@ -22,14 +29,14 @@ app.use('/api/stats', statsRoutes);
 app.get('/', (req, res) => {
   res.send({
     activeStatus: true,
-    error:false
-   })
-})
+    error: false
+  });
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 app.listen(PORT, () => {
-    console.log(`✅ Server running on port ${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
 });
